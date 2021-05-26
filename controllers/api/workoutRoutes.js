40c06470085419b6bed1d4e6a2543c0258713ec3
Neil Creveling <neil.creveling@gmail.com>
2021-05-26 15:39:40 -0400

    code method to get workouts on workoutRoutes.js

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,2 +1,9 @@
 const router = require('express').Router();
-const {}
+const { Workout } = require('../../models');
+
+router.get('/', (req, res) =>
+    Workout.find({})
+        .sort({ date: -1 })
+        .then((dbWorkout) => res.json(dbWorkout))
+        .catch((err) => res.status(400).json(err))
+);
