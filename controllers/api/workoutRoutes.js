c57512b724e9a5cbc2b9809bf85337f532d0ace6
Neil Creveling <neil.creveling@gmail.com>
2021-05-26 15:47:41 -0400

    continue coding workoutRoutes.js, create and update workouts

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -7,3 +7,19 @@ router.get('/', (req, res) =>
         .then((dbWorkout) => res.json(dbWorkout))
         .catch((err) => res.status(400).json(err))
 );
+
+router.post('/', ({ body }, res) =>
+    Workout.create(body)
+        .then((dbWorkout) => res.json(dbWorkout))
+        .catch((err) => res.status(400).json(err))
+);
+
+router.put('/:id', ({ params, body }, res) =>
+    Workout.findOneAndUpdate(
+        { _id: params.id },
+        { $push : { exercises: body } },
+        { upsert: true, useFindandModify: false }
+    ).then((updatedWorkout) => res.json(updatedWorkout))
+);
+
+module.exports = router;
