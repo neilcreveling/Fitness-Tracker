9ba90d7dddaa7d37c766457d43ad4010bda6f524
Neil Creveling <neil.creveling@gmail.com>
2021-05-27 11:45:50 -0400

    clean up code in workoutRoutes.js

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,25 +1,40 @@
 const router = require('express').Router();
-const { Workout } = require('../../models');
+const { Exercise, Model } = require('../../models');
 
-router.get('/', (req, res) =>
+router.get('/', (req, res) => {
     Workout.find({})
         .sort({ date: -1 })
-        .then((dbWorkout) => res.json(dbWorkout))
-        .catch((err) => res.status(400).json(err))
-);
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
-router.post('/', ({ body }, res) =>
+router.post('/', ({ body }, res) => {
     Workout.create(body)
-        .then((dbWorkout) => res.json(dbWorkout))
-        .catch((err) => res.status(400).json(err))
-);
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
-router.put('/:id', ({ params, body }, res) =>
+router.put('/:id', ({ params, body }, res) => {
     Workout.findOneAndUpdate(
         { _id: params.id },
         { $push : { exercises: body } },
-        { upsert: true, useFindandModify: false }
-    ).then((updatedWorkout) => res.json(updatedWorkout))
-);
+        { upsert: true, useFindandModify: false },
+        function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(success);
+            }
+        }
+    //).then((updatedWorkout) => res.json(updatedWorkout))
+)});
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    Exercise: require('./exerciseModel'),
+    Exercise: require('./workoutModel'),
 };
